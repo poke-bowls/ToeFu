7c2342915ea5efa48b848423cabe48c8c03f7c67
Micah Koki <micah.koki@gmail.com>
2016-01-23 12:56:01 -1000

    added player 2 and positioned on screen

--- public/js/states/game.js
+++ public/js/states/game.js
@@ -1,19 +1,40 @@
-//class constructor
+(function() {
 
-PocketTanks.Game = function() {
-  this.player_1;
+var INITIAL_POSITIONS = [
+  //player 1
+  {
+    x : 100, y : 100 },
+  //player 2
+  {
+    x : 600, y : 100 }
+];
 
-};
+  //class constructor
+  PocketTanks.Game = function() {
+    this.player_1;
+    this.player_2;
+  };
 
-PocketTanks.Game.prototype.create = function() {
-  this.player_1 = new PocketTanks.Player( this.game, 0 );
-  this.game.add.existing(this.player_1);
-};
+  PocketTanks.Game.prototype.create = function() {
+    this.player_1 = new PocketTanks.Player( this.game, 0 );
+    this.player_2 = new PocketTanks.Player( this.game, 1 );
+    this.game.add.existing(this.player_1);
+    this.game.add.existing(this.player_2);
 
-PocketTanks.Game.prototype.update = function() {
+    //position players
+    this.player_1.x = INITIAL_POSITIONS[0].x;
+    this.player_1.y = INITIAL_POSITIONS[0].y;
+    this.player_2.x = INITIAL_POSITIONS[1].x;
+    this.player_2.y = INITIAL_POSITIONS[1].y;
 
-};
+  };
 
-PocketTanks.Game.prototype.shutdown = function() {
+  PocketTanks.Game.prototype.update = function() {
 
-};
+  };
+
+  PocketTanks.Game.prototype.shutdown = function() {
+
+  };
+
+})();
